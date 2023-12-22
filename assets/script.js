ca1becc0220d0961f639d807f5467a44fbc46307
Yogi Spice <caseynnewman@yahoo.com>
2023-12-21 23:55:37 -0500

    added comments to JS

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//displaying today's date
+
+//dayjs - probs use Now and format, read the docs
+//get today's date in the right format and save it to a variable
+//console.log that variable
+//append to page
+    //create an element
+    //set innerText to that variable I just made
+    //append that to the currentDay div
+
+
+//use dayjs to get the current hour and store it in a variable
+//could use setInterval to make past/present/future dynamic - though not required
+const divs = document.querySelectorAll('.py-3'); //this is an array
+console.log(divs)
+//for loop
+    //get innerText from each div
+    //compare that to the current hour (if statement)
+        //if that innerText is less than current hour
+        //.setAttribute('class', 'row time-block past')
+        //if innerText is equal to the current hour
+        //.setAttribue('class', 'row time-block past')
+        //...
+
+
+//save things in localStorage
+//get value from text input
+//save to local storage (also save the time! as an object)
