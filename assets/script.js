2d6765f83e7d41143b35bf370721d62c9640b287
Yogi Spice <caseynnewman@yahoo.com>
2023-12-26 00:16:12 -0500

    adjusting changeBackground function

--- assets/script.js
+++ assets/script.js
@@ -20,21 +20,23 @@ const setCurrentTime = dayjs().format('HH:mm');
 
 const changeBackground = () => {
     let currentHour = divs.innerText;
+    let currentDiv = [];
     for (let i = 0; i < divs.length; i++) {
-        if (currentHour < setCurrentTime) {
-            divs.setAttribute('class', 'past');
-        } else if (currentHour = setCurrentTime) {
-            divs.setAttribute('class', 'present');
-        } else if (currentHour > setCurrentTime) {
-            divs.setAttribute('class', 'past');
-        }
+        currentDiv.push(divs[i].innerText)
     }
+    if (currentHour < setCurrentTime) {
+        currentDiv.setAttribute('class', 'past');
+    } else if (currentHour = setCurrentTime) {
+        currentDiv.setAttribute('class', 'present');
+    } else if (currentHour > setCurrentTime) {
+        currentDiv.setAttribute('class', 'past');
+        }
 }
 
 changeBackground()
 
 
-console.log (divs.innerText)
+console.log (currentDiv)
 
 
 //use dayjs to get the current hour and store it in a variable
