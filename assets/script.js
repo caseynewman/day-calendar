59dec76c55d9328302b374bd265f8b1ce1996ef2
Yogi Spice <caseynnewman@yahoo.com>
2024-01-01 12:14:01 -0500

    fixed bugs for saving to and getting back from local storage

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,6 @@ const divs = document.querySelectorAll('.py-3');
 const textAreas = document.querySelectorAll('.description');
 const setCurrentTime = dayjs().format('HH:mm');
 const saveButtonArr = document.querySelectorAll('button');
-// let userToDos = JSON.parse(localStorage.getItem('userToDos'));
-
-
-
 
 
 const setCurrentDate = () => {
@@ -39,67 +35,14 @@ const changeBackgroundColor = (hourDiv, textDiv) => {
 
 setBackgrounds()
 
-
-
-
-
-
-
 for (let i = 0; i < saveButtonArr.length; i++) {
     const saveText = () => {
         const text = textAreas[i].value;
-        localStorage.setItem(i, text)
-        console.log(text)
+        localStorage.setItem(i, text);
     }
-
     saveButtonArr[i].addEventListener('click', saveText)
 }
 
 for (let i = 0; i < textAreas.length; i++) {
-    localStorage.getItem(i, text)
-
+    textAreas[i].value = localStorage.getItem(i);
 }
-
-
-
-
-
-// const toDos = {
-//     input: userToDos.value
-// };
-
-// saveButtonArr.forEach((saveButtonArr) => {
-//     saveButtonArr.addEventListener('click', function(event) {
-//         event.preventDefault();      
-//         localStorage.setItem('userToDos', JSON.stringify(toDos));
-//         displayToDos();
-//     })
-// })
-
-// const displayToDos = () => {
-//     if (userToDos !== null) {
-//         userToDos.innerText = toDos.input;
-//     }
-// }
-
-
-// userToDos.appendChild(toDos)
-
-
-
-// const storeToDos = () => {
-//     const toDoItems = document.createElement('p');
-//     toDoItems.innerText = '';
-//     textAreas.appendChild(toDoItems);
-// }
-
-
-
-
-
-
-
-
-//save things in localStorage
-//get value from text input
-//save to local storage (also save the time! as an object)
